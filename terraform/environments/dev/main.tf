c78ee81bff8bba133bc24c62bd5d931abeaebaf2
hgulsah <130411500+hgulsah@users.noreply.github.com>
2026-04-09 20:59:26 -0400

    Use variables for EKS managed node group sizing

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -57,13 +57,13 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.small"]
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
+  default = {
+    instance_types = [var.node_instance_type]
+    min_size       = var.min_size
+    max_size       = var.max_size
+    desired_size   = var.desired_size
   }
+}
 
   tags = {
     Project     = var.project_name
